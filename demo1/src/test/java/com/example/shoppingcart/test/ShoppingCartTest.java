475f8685f0a166a800b7547a2573788799553005
CIIT502PC21 <it@ciit.edu.ph>
2024-07-06 10:31:01 +0800

    edited comment on test file

--- demo1/src/test/java/com/example/shoppingcart/test/ShoppingCartTest.java
+++ demo1/src/test/java/com/example/shoppingcart/test/ShoppingCartTest.java
@@ -38,7 +38,7 @@ public class ShoppingCartTest {
         System.out.println("Executed after each test method in this class.");
     }
 
-// These are the tests taken when testing the program
+// These are the tests taken when testing the program and stuff
     @Test
     public void testAddProduct() {
         cart.addProduct(product1);
